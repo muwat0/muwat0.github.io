8f14ae587d818040124ee05b707970b29ce452f9
Murat <esertarik58@outlook.com>
2024-03-24 22:01:14 +0300

    add social media links

---
+++ links.js
@@ -0,0 +1,20 @@
+const gh = document.getElementById('gh')
+const dc = document.getElementById('dc')
+const tw = document.getElementById('tw')
+const yt = document.getElementById('yt')
+
+gh.addEventListener("click", () => {
+    window.open("https://github.com/muwat0", "_blank")
+})
+
+dc.addEventListener("click", () => {
+    window.open("https://discordapp.com/users/484787312497262595", "_blank")
+})
+
+tw.addEventListener("click", () => {
+    window.open("https://www.google.com/", "_blank")
+})
+
+yt.addEventListener("click", () => {
+    window.open("https://www.google.com/", "_blank")
+})
